eb27456bfc5bf5e719ba37772ad96d4eaf16a244
UWANJYECaritas <rukundocaritas>
2023-06-21 01:19:05 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,12 +10,13 @@ void print_alphabet_x10(void)
 	int i;
 	int j;
 
-	for (j = 1; i <= 9; i++)
+	for (j = 1; i <= 10; i++)
 	{
 		for (j = 97; j <= 122; j++)
 		{
 			_putchar(j);
 		}
 		_putchar('\n');
+		return (0)
 	}
 }
