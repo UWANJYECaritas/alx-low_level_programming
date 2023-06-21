6a9ec315c6bfc3542ff0c0ec4a59ef1e5a2aec97
UWANJYECaritas <rukundocaritas>
2023-06-20 22:35:37 -0700

    1-alphabatet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase.
+ *
+ * Return: Always 0 (success)
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for  (i = 'a'; i <= 'b'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
