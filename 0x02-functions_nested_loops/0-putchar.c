94e8a6522b7d0b4993d6c13057bc978f87f5955c
UWANJYECaritas <rukundocaritas>
2023-06-20 19:54:36 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+ #include "main.h"
+/**
+ * main - Entry point
+ * Desription: 'prints _putcher to stdout'
+ * Return: always 0
+ */
+int main(void)
+{
+	_putcher('_');
+	_putcher('p');
+	_putcher('u');
+	_putcher('t');
+	_putcher('c');
+	_putcher('h');
+	_putcher('e');
+	_putcher('r');
+	_putcher('\n');
+	return (0);
+}
