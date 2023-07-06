faf0cce6d3476fc97c8b71ff0537c603dfb7f150
UWANJYECaritas <rukundocaritas>
2023-07-05 17:34:13 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#define "main.h"
+
+/**
+ * _memset - fills memory with a costant byte
+ * @s: memory area to be filled
+ * @b: char to copy
+ * @n: number of times to copy b
+ *
+ * Return: a pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = o; i < n; i++)
+		s[i] = b;
+	return (s);
+}
+
